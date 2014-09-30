74b860ce2a6b4ed1aa05d9940a78813a691fd37c
RahulKhanna <sociallyrk@gmail.com>
2014-09-30 09:18:37 -0700

    first commit print coordinates

---
+++ lyft.rb
@@ -0,0 +1,14 @@
+# Programming Challenge (Optional)
+
+# Calculate the detour distance between two different rides. Given four latitude / longitude pairs, 
+# where driver one is traveling from point A to point B and driver two is traveling from point C 
+# to point D, write a function (in your language of choice) to calculate the shorter of the detour 
+# distances the drivers would need to take to pick-up and drop-off the other driver.
+
+coordinates =[[1,1],[2,2],[3,3],[4,4]]
+
+def detour(coordinates)
+  print coordinates
+end
+
+detour(coordinates)
